7e727ed0b864ccdcd11009416ea4d41c659ef2f7
Jami Kettunen <jami.kettunen@protonmail.com>
2021-06-21 05:24:41 +0300

    overlay: Introduce host-ssh-pubkey

---
+++ overlay/host-ssh-pubkey/deploy_host.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copy the public SSH key(s) of your host to the rootfs for seamless authentication.
+host_ssh_pubkey() {
+	local pubkeys=(
+		$HOME/.ssh/id_rsa.pub
+	)
+
+	for user in ${usernames[@]}; do
+		local user_home="home/$user"
+		[ "$user" = "root" ] && user_home="root"
+		local ssh_dir="$rootfs_dir"/$user_home/.ssh
+		[ -e "$ssh_dir" ] || $sudo mkdir -p "$ssh_dir"
+		cat ${pubkeys[@]} | $sudo tee -a "$ssh_dir"/authorized_keys >/dev/null
+	done
+}
+host_ssh_pubkey
