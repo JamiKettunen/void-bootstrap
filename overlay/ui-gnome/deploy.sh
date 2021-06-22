af8c47a9ade9ee59f26c0c85ecf4bc8d508664f4
Jami Kettunen <jami.kettunen@protonmail.com>
2021-06-23 02:57:51 +0300

    overlay: Introduce ui-gnome

---
+++ overlay/ui-gnome/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# HACK: Fixes GDM not starting as of 2021-06-17
+# due to an incorrect rpath in cross-compiled gnome-shell-40.2_1
+ln -s / /usr/aarch64-linux-musl
+ln -s / /usr/aarch64-linux-gnu
+
+xbps-install -y dbus-elogind-libs dbus-elogind-x11 xdg-user-dirs-gtk # Desktop backend stuff
+xbps-install -y mesa-dri glxinfo mesa-demos                          # GPU
+xbps-install -y xorg-server-xwayland xf86-video-fbdev                # Base GUI
+
+# A small GNOME desktop with some default applications installed
+xbps-install -y gnome-core pipewire \
+	gnome-tweaks gnome-terminal gnome-system-monitor gnome-screenshot \
+	gnome-disk-utility gnome-clocks gnome-calendar \
+	gedit
+
+enable_sv gdm
