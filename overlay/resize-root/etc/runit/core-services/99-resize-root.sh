ee4b6facffb5198e03a0186d73590c5481fdbb06
Jami Kettunen <jami.kettunen@protonmail.com>
2021-07-05 01:20:52 +0300

    resize-root: Refactor and start supporting F2FS & XFS

--- overlay/resize-root/etc/runit/core-services/99-resize-root.sh
+++ overlay/resize-root/etc/runit/core-services/99-resize-root.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 # Resize rootfs to fill available partition space.
 resize_root() {
-	local root_mount="$(grep ' / ' /proc/mounts | grep -Ev 'loop|nbd')"
-	if [ "$root_mount" ]; then
-		local root_par="$(echo "$root_mount" | cut -d' ' -f1)"
-		local root_fs="$(echo "$root_mount" | cut -d' ' -f3)"
-		if [ "$root_fs" = "ext4" ]; then
-			msg "Resizing $root_fs root filesystem on $root_par..."
-			resize2fs $root_par
-		else
-			msg "Resizing of $root_fs formatted root not supported; ignoring..."
-		fi
-	else
+	local root_mount="$(mount | grep ' / ' | grep -Ev 'loop|nbd')"
+	if [ -z "$root_mount" ]; then
 		msg "Resizing of loop/nbd based root not supported; ignoring..."
+		return
+	fi
+
+	local root_par="$(echo "$root_mount" | cut -d' ' -f1)"
+	local root_fs="$(echo "$root_mount" | cut -d' ' -f5)"
+	local resize_cmd=""
+	case "$root_fs" in
+		ext*) resize_cmd="resize2fs" ;;
+		f2fs) resize_cmd="resize.f2fs" ;;
+		xfs) resize_cmd="xfs_growfs -d" ;;
+		*) msg "Resizing of $root_fs formatted root not supported; ignoring..." ;;
+	esac
+
+	if [ "$resize_cmd" ]; then
+		msg "Resizing $root_fs root filesystem on $root_par..."
+		$resize_cmd $root_par
 	fi
 }
 resize_root; unset resize_root
