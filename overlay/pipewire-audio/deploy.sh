94884908aeb7fbc170055c067450310095baf8ec
Jami Kettunen <jami.kettunen@protonmail.com>
2021-07-28 11:31:31 +0300

    overlay: Introduce pipewire-audio

---
+++ overlay/pipewire-audio/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+xbps-install -y pipewire alsa-pipewire rtkit pulseaudio-utils alsa-utils
+
+# Setup BT bits conditionally
+if command -v bluetoothctl >/dev/null; then
+	xbps-install -y libspa-bluetooth
+fi
+
+# Setup ALSA bits
+mkdir -p /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
+ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+
+# TODO: JACK replacement (conditionally)?
