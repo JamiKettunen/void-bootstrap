7024989a58fc8eabcf5ad67c9bb9490a1f1edbe0
Jami Kettunen <jami.kettunen@protonmail.com>
2022-02-21 13:18:25 +0200

    tethering: Fix script under Void Linux

--- tethering.sh
+++ tethering.sh
@@ -2,7 +2,8 @@
 set -e
 # TODO: use nftables instead?
 sudo sysctl net.ipv4.ip_forward=1 >/dev/null
-sudo iptables -P FORWARD ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -s 172.16.42.0/24 -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 172.16.42.0/24
 cat <<'EOF'
 >> Now run 'ip route add default via 172.16.42.2 dev usb0' on your device!
