3aa13902c046eef76204dc5e1168bca04121494f
Jami Kettunen <jami.kettunen@protonmail.com>
2022-09-12 00:36:41 +0300

    tethering: Automatically start rndis-tethering service if possible
    This needs SSH key-based authentication setup however unless also
    configured with permit_root_login=true.

--- tethering.sh
+++ tethering.sh
@@ -5,7 +5,15 @@ sudo sysctl net.ipv4.ip_forward=1 >/dev/null
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A FORWARD -s 172.16.42.0/24 -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 172.16.42.0/24
-cat <<'EOF'
+
+if ip a | grep -q 'inet 172.16.42.2'; then
+	ssh root@172.16.42.1 sv start rndis-tethering
+else
+	cat <<'EOF'
 >> Now run 'ip route add default via 172.16.42.2 dev usb0' on your device!
    (running 'ip route del default via 172.16.42.2' will undo this)
+
+>> With 'usbd' package 'sv start rndis-tethering' & 'sv stop rndis-tethering'
+   will work as well.
 EOF
+fi
