b3d4559dad9edf7a56322eb45eee3b04c2c9b63d
Jami Kettunen <jami.kettunen@protonmail.com>
2022-09-11 23:32:17 +0300

    overlay: ui-gnome: Drop aarch64 rpath hack from 15 months ago
    This is no longer needed to launch GDM/GNOME, hooray!

--- overlay/ui-gnome/deploy.sh
+++ overlay/ui-gnome/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-
-# HACK: Fixes GDM not starting as of 2021-06-17
-# due to an incorrect rpath in cross-compiled gnome-shell-40.2_1
-ln -s / /usr/aarch64-linux-musl
-ln -s / /usr/aarch64-linux-gnu
-
 xbps-install -y dbus-elogind-libs dbus-elogind-x11 xdg-user-dirs-gtk # Desktop backend stuff
 xbps-install -y mesa-dri glxinfo mesa-demos                          # GPU
 xbps-install -y xorg-server-xwayland xf86-video-fbdev                # Base GUI
