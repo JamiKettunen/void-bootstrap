57e7083223aaf827dd30403febccc3913b4c8e64
Jami Kettunen <jami.kettunen@protonmail.com>
2022-10-09 02:54:55 +0300

    tethering.sh: Make a bit more customizable & account for sv start fail

--- tethering.sh
+++ tethering.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 set -e
+: ${RNDIS_USB_NET:=172.16.42.0/24}
+: ${RNDIS_USB_HOST:=172.16.42.2}
+: ${RNDIS_USB_DEVICE:=172.16.42.1}
+
 # TODO: use nftables instead?
 sudo sysctl net.ipv4.ip_forward=1 >/dev/null
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A FORWARD -s 172.16.42.0/24 -j ACCEPT
-sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 172.16.42.0/24
+sudo iptables -A FORWARD -s $RNDIS_USB_NET -j ACCEPT
+sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s $RNDIS_USB_NET
 
-if ip a | grep -q 'inet 172.16.42.2'; then
-	ssh root@172.16.42.1 sv start rndis-tethering
-else
+MANUAL_STEPS_NEEDED=1
+if ip a | grep -q "inet $RNDIS_USB_HOST"; then
+	ssh root@$RNDIS_USB_DEVICE sv start rndis-tethering && MANUAL_STEPS_NEEDED=0
+fi
+
+if [ $MANUAL_STEPS_NEEDED -eq 1 ]; then
 	cat <<'EOF'
->> Now run 'ip route add default via 172.16.42.2 dev usb0' on your device!
-   (running 'ip route del default via 172.16.42.2' will undo this)
+>> Now run 'ip route add default via $RNDIS_USB_HOST dev usb0' on your device!
+   (running 'ip route del default via $RNDIS_USB_HOST' will undo this)
 
 >> With 'usbd' package 'sv start rndis-tethering' & 'sv stop rndis-tethering'
    will work as well.
