c23820e92bda86052df9d82c5d072062d9541b0c
Jami Kettunen <jami.kettunen@protonmail.com>
2022-12-19 21:03:45 +0200

    config: Update to 20221001 release base images

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 # Void Linux release tarballs to target
 # e.g. "current" or "20210316"
 # https://alpha.de.repo.voidlinux.org/live
-release="20210930"
+release="20221001"
 
 # Backend to use for executing scripts and commands on the rootfs; supported choices include:
 # "chroot"
