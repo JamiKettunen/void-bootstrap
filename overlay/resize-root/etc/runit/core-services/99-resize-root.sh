028491e234f8ede49e21b47f2d7cfd7a12939890
Jami Kettunen <jami.kettunen@protonmail.com>
2022-12-21 03:08:35 +0200

    resize-root: Don't try to resize rootfs image files
    They won't exist anyway on the seen root_par once switch_root is done.

--- overlay/resize-root/etc/runit/core-services/99-resize-root.sh
+++ overlay/resize-root/etc/runit/core-services/99-resize-root.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 # Resize rootfs to fill available partition space.
 resize_root() {
 	local root_mount="$(mount | grep ' / ' | grep -Ev 'loop|nbd')"
@@ -17,10 +16,15 @@ resize_root() {
 		*) msg "Resizing of $root_fs formatted root not supported; ignoring..." ;;
 	esac
 
-	if [ "$resize_cmd" ]; then
-		msg "Resizing $root_fs root filesystem on $root_par..."
-		$resize_cmd $root_par
-	fi
+	case "$root_par" in
+		*.img) msg "Resizing of rootfs image files not supported; ignoring..." ;;
+		*)
+			if [ "$resize_cmd" ]; then
+				msg "Resizing $root_fs root filesystem on $root_par..."
+				$resize_cmd $root_par
+			fi
+		;;
+	esac
 }
 resize_root; unset resize_root
 rm /etc/runit/core-services/99-resize-root.sh
