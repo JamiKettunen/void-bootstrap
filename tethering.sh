b6daae9cf4bbdf8f4b127e8cbc23dd1ce618bc5b
Jami Kettunen <jami.kettunen@protonmail.com>
2023-07-04 17:17:07 +0300

    tethering: Silence iptables output

--- tethering.sh
+++ tethering.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
-# TODO: Check if already done!
-sudo sysctl net.ipv4.ip_forward=1
+# TODO: use nftables instead?
+sudo sysctl net.ipv4.ip_forward=1 >/dev/null
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 172.16.42.0/24
-echo ">> Now run 'ip route add default via 172.16.42.2 dev usb0' on your device!"
-echo "   (running 'ip route del default via 172.16.42.2' will undo this)"
+cat <<'EOF'
+>> Now run 'ip route add default via 172.16.42.2 dev usb0' on your device!
+   (running 'ip route del default via 172.16.42.2' will undo this)
+EOF
