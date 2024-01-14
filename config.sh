56e855949da5fd33952f2f2a8754d2a4eeb92f9d
Jami Kettunen <jami.kettunen@protonmail.com>
2024-01-14 15:15:27 +0200

    config: Drop remaining reference to old now dead mirror
    https://voidlinux.org/news/2023/10/repo-changes.html

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ sudo_timer=false
 
 # Void Linux release tarballs to target
 # e.g. "current" or "20210316"
-# https://alpha.de.repo.voidlinux.org/live
+# https://repo-default.voidlinux.org/live
 release="20230628"
 
 # Backend to use for executing scripts and commands on the rootfs; supported choices include:
